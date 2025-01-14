fdba7cc32dac0bd82d18e2a1c23414d92bd2044d
Ryuivan <ryu.ivan@student.umn.ac.id>
2025-01-14 19:31:10 +0700

    Initial commit

---
+++ src/main/java/ryu/spring/ems/Learn/Spring/Boot/with/PostgreSQL/LearnSpringBootWithPostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package ryu.spring.ems.Learn.Spring.Boot.with.PostgreSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootWithPostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootWithPostgreSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ryu/spring/ems/Learn/Spring/Boot/with/PostgreSQL/LearnSpringBootWithPostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package ryu.spring.ems.Learn.Spring.Boot.with.PostgreSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootWithPostgreSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
